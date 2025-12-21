a3b9f2eef07d835159aa97a7b7713a5fdfe37b27
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-12-21 16:11:18 +0530

    engine: core[GameLoop]

--- engine/core/GameLoop.cpp
+++ engine/core/GameLoop.cpp
@@ -1,9 +1,11 @@
 #include "GameLoop.hpp"
 #include "Time.hpp"
 #include "Logger.hpp"
-#include "engine/platform/Window.hpp"
 #include "Config.hpp"
+
+#include "engine/platform/Window.hpp"
 #include "game/world/ChunkManager.hpp"
+#include "game/world/Mesher.hpp"
 
 void GameLoop::run() {
     Window window(
@@ -13,6 +15,7 @@ void GameLoop::run() {
     );
 
     ChunkManager world(1337);
+    Mesher mesher;
 
     Time::init();
 
@@ -21,6 +24,13 @@ void GameLoop::run() {
 
         world.update(playerChunkX, playerChunkZ);
         playerChunkX++;
+
+        world.forEachChunk([&](Chunk& chunk) {
+            if (chunk.dirty) {
+                mesher.buildMesh(chunk);
+                chunk.dirty = false;
+            }
+        });
 
         window.pollEvents();
     }
