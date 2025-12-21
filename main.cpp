4effe3dee0501345031632d8cfb8cebc27ee7358
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-12-21 14:16:43 +0530

    main: main endpoint

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "engine/core/Application.hpp"
+#include "engine/threading/JobSystem.hpp"
+#include "game/world/BlockRegistry.hpp"
+#include <thread>
+
+int main() {
+    BlockRegistry::init();
+    JobSystem::init(std::thread::hardware_concurrency());
+
+    Application app;
+    app.run();
+    
+    JobSystem::shutdown();
+    return 0;
+}
