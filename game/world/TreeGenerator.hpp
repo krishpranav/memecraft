3f11f03d8ff8427eb113d39c469aafbfc09ec21c
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-12-23 11:42:07 +0530

    game: world[TreeGenerator]

---
+++ game/world/TreeGenerator.hpp
@@ -0,0 +1,11 @@
+//
+// Created by Krisna Pranav on 21/12/25.
+//
+
+#pragma once
+#include "Chunk.hpp"
+
+class TreeGenerator {
+public:
+    static void placeTree(Chunk& chunk, int x, int y, int z);
+};
